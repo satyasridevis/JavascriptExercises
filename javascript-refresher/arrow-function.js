901c0e1107608d3406400af4128bd9196969139e
satyasridevis <satyasridevisk@gmail.com>
2022-12-23 12:44:31 +0530

    course: revises ES6 concepts

---
+++ javascript-refresher/arrow-function.js
@@ -0,0 +1,27 @@
+function printMyFirstName(fname) {
+    console.log(fname)
+}
+
+printMyFirstName("satya");
+
+
+const printMySecondName = (sname) =>
+{
+    console.log(sname)
+
+}
+
+printMySecondName("sridevi")
+
+
+// const multiply = (number) => {
+//     return number * 2;
+// }
+
+// console.log(multiply(2))
+
+const multiply = number => number * 2;
+
+console.log(multiply(3))
+
+
---
+++ javascript-refresher/let-const.js
@@ -0,0 +1,22 @@
+var myName = 'Max';
+
+console.log(myName)
+
+myName = 'manu';
+
+console.log(myName)
+
+let nameone = "satya"
+
+console.log(nameone)
+
+nameone = "sridevi"
+
+console.log(nameone)
+
+const nametwo = "deekshaya"
+
+// 
+
+console.log(nametwo)
+
